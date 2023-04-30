3ca7f1f164d3bdc5886f295bdc0131c09c20bf46
eg <eg104134@163.com>
2023-04-30 16:52:30 +0800

    分类页面左侧导航栏更新

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,63 @@
 <template>
-  <h2>category</h2>
+  <div id="category">
+    <nav-bar class="nav-bar">
+      <template #center>分类</template>
+    </nav-bar>
+    <div class="category-content">
+      <b-scroll ref="left-scroll">
+        <category-title-list :titleList="leftTitleList"></category-title-list>
+      </b-scroll>
+      <b-scroll ref="right-scroll">
+        <category-detail-list></category-detail-list>
+      </b-scroll>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "Category"
-}
+import {getCategoryTitle} from "@/network/category";
+
+import NavBar from "components/common/navbar/NavBar.vue";
+import BScroll from "components/common/scroll/BScroll.vue";
+
+import CategoryTitleList from "./childComponents/CategoryTitleList.vue";
+import CategoryDetailList from "./childComponents/CategoryDetailList.vue";
+
+  export default {
+    name: "Category",
+    components: {
+      NavBar,
+      BScroll,
+      CategoryTitleList,
+      CategoryDetailList
+    },
+    data() {
+      return {
+        leftTitleList: []
+      }
+    },
+
+    mounted() {
+      getCategoryTitle().then(res => {
+        this.leftTitleList = res.data.category.list
+        console.log('category title', res)
+      })
+    }
+  }
 </script>
 
 <style scoped>
-
+  #category {
+    height: 100vh;
+    width: 100%;
+  }
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+  .category-content {
+    display: flex;
+    height: calc(100% - 44px - 49px);
+    overflow: hidden;
+  }
 </style>
---
+++ src/views/category/childComponents/CategoryTitleList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div v-for="(item, index) in titleList" class="title-list-item"
+         :class="{isActive: currentIndex === index}" @click="itemClick(index)">
+      {{item.title}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CategoryTitleList",
+  props: {
+    titleList: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  methods: {
+    itemClick(index) {
+      this.currentIndex = index
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title-list-item {
+    width: 90px;
+    height: 40px;
+    line-height: 40px;
+    border-bottom: #f2f2f2 2px solid;
+    background-color: #efefef;
+    text-align: center;
+    font-size: 14px;
+  }
+  .isActive {
+    background-color: #fff;
+    border-left: var(--color-tint) 3px solid;
+    color: var(--color-high-text);
+  }
+</style>
